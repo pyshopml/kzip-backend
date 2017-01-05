8a0c92b31f9b32de48cb7e9164f6e0640b10c64f
vetos_88 <symphonyofbody@gmail.com>
2017-01-05 15:00:28 +0300

    KZIP-004: kzapp add Comment model register Comment model in admin

--- kzipproj/kzip/admin.py
+++ kzipproj/kzip/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from .models import Comment
+
+
+@admin.register(Comment)
+class CommentAdmin(ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'user',
+        'create_date',
+    ]
+
+    list_display_links = [
+        'id',
+        'title',
+    ]
--- kzipproj/kzip/models.py
+++ kzipproj/kzip/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name='Пользователь',
+        # default=ExtUser.objects.get(login='anonymous'),
+    )
+    title = models.CharField(
+        'Название',
+        max_length=255,
+    )
+    comment = models.TextField(
+        'Отзыв',
+    )
+    create_date = models.DateTimeField(
+        'Создан',
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        object_name = "{}-{}-{}".format(self.create_date, self.user.email, self.title)
+        return object_name
+
+    def get_user(self):
+        user = "{}".format(self.user.email)
+        return user
+
+    class Meta:
+        ordering = ["-create_date", "user"]
+        get_latest_by = "create_date"
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
