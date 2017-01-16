fd69a1711c005113101faafc0b54942c0de8a1e6
vetos_88 <symphonyofbody@gmail.com>
2017-01-16 17:11:03 +0300

    Fix. add IsAuthenticatedOrReadOnly to publications view

--- kzipproj/kzip/views.py
+++ kzipproj/kzip/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .permissions import IsOwnerOrReadOnly
 from .models import Publication
@@ -13,7 +14,7 @@ class PublicationViewSet(viewsets.ModelViewSet):
     queryset = Publication.objects.all().order_by('-create_date')
     serializer_class = PublicationSerializer
     authentication_classes = (SessionAuthentication, BasicAuthentication)
-    permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
