24867ac5c23fa8f9d046bd099c90cadfc8ff119f
vetos_88 <symphonyofbody@gmail.com>
2017-01-25 13:01:40 +0300

    KZIP-016: merge develop into

--- kzipproj/users/tests/tests_views.py
+++ kzipproj/users/tests/tests_views.py
@@ -6,10 +6,13 @@ from .. import consts
 
 
 class LoginViewTestCase(TestCase):
+    def setUp(self):
+        self.login_path = reverse('users:login')
+
     def test_ok_login_successful(self):
         email, password = 'sym1@gmail.com', 'qweasdzxc'
         user = ExtUser.objects.create_user(email=email, password=password, is_active=True)
-        response = self.client.post(path=reverse('login'),
+        response = self.client.post(path=self.login_path,
                                     data={
                                         'email': email,
                                         'password': password
@@ -22,8 +25,7 @@ class LoginViewTestCase(TestCase):
     def test_fail_login_inactive_user(self):
         email, password = 'sym2@gmail.com', 'qweasdzxc'
         user = ExtUser.objects.create_user(email=email, password=password, is_active=False)
-        print(user.is_active)
-        response = self.client.post(path=reverse('login'),
+        response = self.client.post(path=self.login_path,
                                     data={
                                         'email': email,
                                         'password': password
@@ -33,16 +35,20 @@ class LoginViewTestCase(TestCase):
         self.assertEqual(response.data['message'], consts.INACTIVE_ACCOUNT)
 
     def test_fail_incorrect_credentials(self):
-        response = self.client.post(path=reverse('login'),
+        response = self.client.post(path=self.login_path,
                                     data={'email': 'email', 'password': 'password'})
 
         self.assertEquals(response.status_code, 400)
         self.assertNotIn('_auth_user_id', self.client.session)
         self.assertEqual(response.data['message'], consts.INVALID_CREDENTIALS)
 
-        response = self.client.post(path=reverse('login'),
+        response = self.client.post(path=self.login_path,
                                     data={'password': 'password'})
 
         self.assertEquals(response.status_code, 400)
         self.assertNotIn('_auth_user_id', self.client.session)
         self.assertEqual(response.data['message'], consts.INVALID_CREDENTIALS)
+
+    def test_ok_logout(self):
+        pass
+
